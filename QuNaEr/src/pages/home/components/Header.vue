f8acb3614fed9605faa15161de8488b548894f25
debug <18161975696@163.com>
2018-11-08 18:26:15 +0800

    以前代码全删了，一步搞错，有多搞了1小时

---
+++ QuNaEr/src/pages/home/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点
+    </div>
+    <div class="header-right">
+      <span>城市</span>
+      <span class="iconfont arrow-icon">&#xe601;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles'
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      color: #ccc
+      margin: .12rem .2rem
+      background: #fff
+      border-radius: .1rem
+      padding-left: .1rem
+      span
+        font-size: 0.3125rem
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      span
+        font-size: 0.28 rem
+      .arrow-icon
+        font-size: .24rem
+
+</style>
