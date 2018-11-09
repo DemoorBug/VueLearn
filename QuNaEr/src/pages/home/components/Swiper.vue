42027c2e81db013e3dbdba61eb53356056d3516b
debug <18161975696@163.com>
2018-11-09 17:40:44 +0800

    change

---
+++ QuNaEr/src/pages/home/components/Swiper.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key='item.id'>
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/c6/2467595fffc3b302.jpg_750x200_cca13d51.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/8d/f71f1f07ced20802.jpg_750x200_1aed3b5a.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.55%
+    .swiper-img
+      width: 100%
+</style>
